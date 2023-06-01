2a46956b2334bc1906fcdb32b07b327c3a009dd6
pedroF2P <pedroangel011@hotmail.com>
2023-06-01 13:44:01 -0600

    correccion1

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,9 @@
+const Title = () => {
+    return (
+        <h1 className="text-5xl font-anton font-bold tracking-widest ">
+            Todo List
+        </h1>
+    );
+}
+<style>text-align: center</style>
+export { Title }
